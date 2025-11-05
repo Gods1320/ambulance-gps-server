2bad9316e5f67b16788cbe833ce5d92f2af97470
Gods1320 <geshybarbia@gmail.com>
2025-11-05 23:35:23 +0800

    Clean env vars and use Railway DB

--- index.js
+++ index.js
@@ -12,7 +12,10 @@ const db = mysql.createPool({
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT
+  port: process.env.MYSQLPORT,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 // ✅ Test DB Connection
